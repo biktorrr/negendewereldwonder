afe240d69cb1f5796e11ba1b516ab3ccae51bec9
Manon <pepijn@synergique.nl>
2013-05-22 20:18:09 +0200

    - Added vlc-based compilation player

---
+++ compilationPlayer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import time
+import subprocess
+import shlex
+import os
+from glob import glob
+import logging
+import sys
+from shutil import copy
+from os.path import join,basename
+
+videoCompilationPath = "/home/synergique/videoCompilation"
+repeat = 4
+
+class CompilationPlayer:
+	videoCompilationPath = None
+	repeat = None
+	
+	def __init__(self, videoCompilationPath, repeat):
+		self.videoCompilationPath = videoCompilationPath
+		self.repeat = repeat
+
+	def updatePlaylist(self):
+		out = open(self.videoCompilationPath+"/playlist.m3u", "w");
+
+		for videoFile in glob(self.videoCompilationPath+"/*.avi"):
+			for i in range(self.repeat):
+				out.write(videoFile+"\n")
+
+		out.close()
+
+	def main(self):
+		logging.basicConfig(
+		    filename='/home/synergique/mplayercontroller.log', 
+		    filemode='w', 
+		    format='%(asctime)s %(message)s', 
+		    level=logging.DEBUG
+		)
+
+		while True:
+			self.updatePlaylist()
+			command = 'cvlc -f -L -q --no-osd --no-video-title-show "%s/playlist.m3u"' % (self.videoCompilationPath)
+			print command
+			subprocess.call(shlex.split(command))
+
+if __name__ == "__main__":
+    CompilationPlayer(videoCompilationPath, repeat).main()
+
